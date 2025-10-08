fd1985ab79cf8ad6b57a2aecf2fa42208de38279
sayantansantra <sayantan353@gmail.com>
2025-10-08 19:47:12 +0530

    1st check in

---
+++ src/main/java/com/say/CheckGitAction/CheckGitActionApplication.java
@@ -0,0 +1,13 @@
+package com.say.CheckGitAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckGitActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckGitActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/say/CheckGitAction/CheckGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.say.CheckGitAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckGitActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
